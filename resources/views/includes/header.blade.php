7bf1ec9aaaadc363e7571ecd37ea36ef5e082efe
Kevin Glick <kevin@grizzdev.com>
2015-08-16 15:56:30 -0700

    fixed shipping calculation added Bracelet images fixed printable order confirmation

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -1,3 +1,6 @@
 <header>
-@include('includes.nav')
+	@include('includes.nav')
+	<div class="visible-print-block text-center">
+		<img src="{{ asset('img/logo_250x225.png') }}" alt="Gamerosity" style="width:125px" />
+	</div>
 </header>
--- resources/views/shop/order.blade.php
+++ resources/views/shop/order.blade.php
@@ -1,12 +1,15 @@
 @extends('layouts.master')
 
 @section('content')
-<div class="row checkout-form">
-	<div class="col-sm-12 col-md-10 col-md-offset-1 pt-120 pb-40">
-		<h1>Thank you for your Order!</h1>
+<div class="hidden-print pt-120"></div>
+<div class="row order-content">
+	<div class="col-sm-12 col-md-10 col-md-offset-1 pb-40">
+		<h1 class="hidden-print hidden-xs">Thank you for your Order!</h1>
+		<h3 class="hidden-print visible-xs">Thank you for your Order!</h3>
+		<p class="visible-print-block">Thank you for your Order!</p>
 		<div class="row">
-			<div class="col-sm-12 col-md-6">
-				<div class="panel panel-default">
+			<div class="col-xs-12 col-sm-6">
+				<div class="panel panel-primary">
 					<div class="panel-heading">
 						<h5>Billing Details</h5>
 					</div>
@@ -30,8 +33,8 @@
 					</div>
 				</div>
 			</div>
-			<div class="col-sm-12 col-md-6">
-				<div class="panel panel-default">
+			<div class="col-xs-12 col-sm-6">
+				<div class="panel panel-primary">
 					<div class="panel-heading">
 						<h5>Shipping Details</h5>
 					</div>
@@ -55,10 +58,8 @@
 					</div>
 				</div>
 			</div>
-		</div>
-		<div class="row">
-			<div class="col-sm-12 col-md-6">
-				<div class="panel panel-default">
+			<div class="col-xs-12 col-sm-6">
+				<div class="panel panel-primary">
 					<div class="panel-heading">
 						<h5>Personal Details</h5>
 					</div>
@@ -108,8 +109,8 @@
 					</div>
 				</div>
 			</div>
-			<div class="col-sm-12 col-md-6">
-				<div class="panel panel-default">
+			<div class="col-xs-12 col-sm-6">
+				<div class="panel panel-primary">
 					<div class="panel-heading">
 						<h5>Order Details</h5>
 					</div>
@@ -146,58 +147,56 @@
 								{{ ($checkout['payment-type'] == 'paypal') ? 'PayPal' : 'Credit Card' }}
 							</div>
 						</div>
-						<div class="row">
-							<div class="col-xs-12 table-responsive">
-								<br />
-								<table class="table table-striped">
-									<thead>
-										<tr>
-											<th>Item</th>
-											<th>Price</th>
-											<th>Quantity</th>
-											<th>Total</th>
-										</tr>
-									</thead>
-									<tbody>
-										@foreach($cart as $item)
-										<tr>
-											<td>
-												<b>{{ $item->name }}</b>
-										@foreach($item->attributes as $attr)
-											@if($attr->name != 'Amount')
-												<br />
-												<small>
-													<b>{{ $attr->name }}:</b>
-													{{ $attr->value }}
-												</small>
-											@else
-												<?php $item->price += $attr->value ?>
-											@endif
-										@endforeach
-											</td>
-											<td>${{ number_format($item->price, 2, '.', '') }}</td>
-											<td align="center">{{ $item->quantity }}</td>
-											<td align="right">${{ number_format($item->price * $item->quantity, 2, '.', '') }}</td>
-										</tr>
-										@endforeach
-									</tbody>
-									<tfoot>
-										<tr>
-											<th colspan="2" rowspan="3"></th>
-											<td align="right">Subtotal</td>
-											<td align="right">${{ $checkout['total'] }}
-										</tr>
-										<tr>
-											<td align="right">Shipping</td>
-											<td align="right">{{ (!empty($checkout['shipping'])) ? $checkout['shipping'] : 'FREE' }}</td>
-										</tr>
-										<tr>
-											<td align="right">Total</td>
-											<td align="right">${{ number_format($checkout['total'], 2, '.', '') }}</td>
-										</tr>
-									</tfoot>
-								</table>
-							</div>
+						<div class="row table-responsive">
+							<br />
+							<table class="table table-striped">
+								<thead>
+									<tr>
+										<th>Item</th>
+										<th>Price</th>
+										<th>Quantity</th>
+										<th>Total</th>
+									</tr>
+								</thead>
+								<tbody>
+									@foreach($cart as $item)
+									<tr>
+										<td>
+											<b>{{ $item->name }}</b>
+									@foreach($item->attributes as $attr)
+										@if($attr->name != 'Amount')
+											<br />
+											<small>
+												<b>{{ $attr->name }}:</b>
+												{{ $attr->value }}
+											</small>
+										@else
+											<?php $item->price += $attr->value ?>
+										@endif
+									@endforeach
+										</td>
+										<td>${{ number_format($item->price, 2, '.', '') }}</td>
+										<td align="center">{{ $item->quantity }}</td>
+										<td align="right">${{ number_format($item->price * $item->quantity, 2, '.', '') }}</td>
+									</tr>
+									@endforeach
+								</tbody>
+								<tfoot>
+									<tr>
+										<th colspan="2" rowspan="3"></th>
+										<td align="right">Subtotal</td>
+										<td align="right">${{ $checkout['total'] }}
+									</tr>
+									<tr>
+										<td align="right">Shipping</td>
+										<td align="right">{{ (!empty($checkout['shipping'])) ? $checkout['shipping'] : 'FREE' }}</td>
+									</tr>
+									<tr>
+										<td align="right">Total</td>
+										<td align="right">${{ number_format($checkout['total'], 2, '.', '') }}</td>
+									</tr>
+								</tfoot>
+							</table>
 						</div>
 					</div>
 				</div>
