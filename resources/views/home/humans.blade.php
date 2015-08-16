67dcd687cd025292a6aee6557e5d93467af58465
Kevin Glick <kevin@grizzdev.com>
2015-08-15 19:44:45 -0700

    500 error view, humans.txt

---
+++ resources/views/home/humans.blade.php
@@ -0,0 +1,22 @@
+/* TEAM */
+	Designer: Manny Munoz
+	Contact: manny [at] gamerosity.com
+	Twitter: @gamerosity
+	From: Medford, OR, USA
+
+	Developer: Kevin Glick
+	Contact: kevin [at] grizzdev.com
+	Twitter: @grizzdev
+	From: Ashland, OR, USA
+
+/* THANKS */
+	Laravel God: Taylor Otwell
+	Twitter: @taylorotwell
+	From: Heaven
+
+/* SITE */
+	Last Update: 2015/08/15
+	Language: English
+	Doctype: HTML5
+	IDE: Sublime Text 3, Navicat, Transmit, Photoshop
+	Technology: Laravel 5.1, jQuery/UJS, Bootstrap 3
