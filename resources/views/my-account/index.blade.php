600f69604bf2a7e244efa9b8033fe1a1083bb313
Kevin Glick <kevin@grizzdev.com>
2015-08-20 00:03:59 -0700

    fixed typo

--- resources/views/my-account/index.blade.php
+++ resources/views/my-account/index.blade.php
@@ -4,7 +4,6 @@
 <div class="account-content pt-100 pb-40">
 	<div class="row">
 		<div class="col-sm-12 col-md-8 col-md-offset-2">
-		{!!  !!}
 		</div>
 	</div>
 	@include('includes.account-orders', [
